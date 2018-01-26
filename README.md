e7e96dc5705f5ae74396137034c09fec40555f28
Line Moseng <line@vibbio.com>
2018-01-26 17:08:35 +0100

    Get endpoints

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # festivalbingo-service
+
+This is the festival bingo backend service.
+
+```
+users (id, username, email, game)
+games (id, name)
+boards (id, ownerid, gameid, dimension)
+tiles (id, boardid, xpos, ypos)
+fields (id, tileid, rowid, ar   tist)
+```
---
+++ src/mockDB.js
@@ -0,0 +1,76 @@
+module.exports = {
+    deletePart: function (serialnumber, cb) {
+        return true;
+    },
+
+    updateTile: function (boardId, fieldId, json, cb) {
+        console.log(boardId, fieldId, json);
+        cb()
+    },
+
+    addUser: function (updateInfo, cb) {
+        cb();
+    },
+
+    getUser: function (boardId, username, cb) {
+        var user = `{
+            "username": "${username}",
+            "email": "test@test.no"
+        }`;
+        cb(user);
+    },
+
+    getAllUsers: function (cb) {
+        var users = `[{
+            "username": "linepine",
+            "email": "test@test.no",
+            "gameId": 1
+        }, {
+            "username": "pingrid",
+            "email": "test@test.no",
+            "gameId": 1
+        }]`;
+        cb(users);
+    },
+
+    getBoard: function (boardId, cb) {
+        var tiles = [];
+        var tileId = 0;
+        var fieldId = 0;
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                tiles.push(` {
+                    "tileId": ${tileId},
+                    "posy": ${i},
+                    "posx": ${j},
+                    "fields": [
+                        {
+                            "rowNumber": 0,
+                            "artist": "Lars Vaular",
+                            "fileId": ${Math.floor(Math.random() * Math.floor(234567))}
+                        },
+                        {
+                            "rowNumber": 1,
+                            "artist": "Kygo",
+                            "fileId": ${Math.floor(Math.random() * Math.floor(234567))}
+                        },
+                        {
+                            "rowNumber": 2,
+                            "artist": "Foo Fighters",
+                            "fileId": ${Math.floor(Math.random() * Math.floor(234567))}
+                        }
+                    ]
+                }`);
+                tileId++;
+            }
+        }
+
+        cb(`{
+            "id": ${boardId},
+            "dimensions": 5,
+            "tiles": 
+               ${tiles}
+            
+        }`);
+    }
+};
